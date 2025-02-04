504a9d0a4cb21bfabf3d20ec80fc1f80d42f388f
adeojoemmanueli <emmanuel@xroads.tech>
2025-02-04 17:33:19 +0100

    feat: seed data to medication

--- src/migrations/2712345678901-SeedMedications.ts
+++ src/migrations/2712345678901-SeedMedications.ts
@@ -47,6 +47,15 @@ export class CreateMedicationTable1699999999999 implements MigrationInterface {
         onDelete: 'SET NULL',
       }),
     );
+
+    await queryRunner.query(`
+      INSERT INTO medication (name, weight, code, image, droneId) VALUES
+      ('Painkiller', 50, 'PK_001', 'image1.jpg', (SELECT id FROM drone WHERE serial_number = 'DRN_1X92L')),
+      ('Antibiotic', 75, 'AB_002', 'image2.jpg', (SELECT id FROM drone WHERE serial_number = 'DRN_5T43M')),
+      ('Vaccine', 100, 'VC_003', 'image3.jpg', (SELECT id FROM drone WHERE serial_number = 'DRN_9K76H')),
+      ('Insulin', 90, 'IN_004', 'image4.jpg', (SELECT id FROM drone WHERE serial_number = 'DRN_3P29C')),
+      ('Cough Syrup', 60, 'CS_005', 'image5.jpg', (SELECT id FROM drone WHERE serial_number = 'DRN_7R82L'));
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
